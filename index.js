3f0a0e85a5beb4ad28167707af358e88e015206d
Huseyn Abbasov <nelbekistekan12@gmail.com>
2023-10-05 21:55:25 +0400

    Done

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import path from 'path'
 
-import {rename} from "node:fs/promises"
+import {rename,appendFile,readdir} from "node:fs/promises"
 import { fileURLToPath } from 'url'
 
 const __filename  = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-let fullPath = path.join(__dirname,"files","json.txt")
+// let fullPath = path.join(__dirname,"files","json.txt")
+// appendFile()
+// rename(fullPath,fullPath.replace("json.txt","user.txt"))
 
-rename(fullPath,fullPath.replace("json.txt","user.txt"))
+let id = 1;
+let dirPath = path.join(__dirname,"anonymous")
+readdir(dirPath).then((result)=>{
+    result.forEach(element => {
+        let fullPath = path.join(__dirname,"anonymous",`${element}`)
+        rename(fullPath,fullPath.replace(`${element}`,`file${id}.json`))
+        id++;
+    });
+});
+
+
